488d30482cd1ea72b1a69fcddac6a86d5089117f
HelloTakako <24834291+HelloTakako@users.noreply.github.com>
2019-04-12 21:20:13 -0700

    added test function

--- src/0_init.js
+++ src/0_init.js
@@ -1,7 +1,18 @@
 console.log("Hello world!");
-function あ() {
-}
+function あ() {}
 
 あ.parseBOB = function(string) {
   return JSON.parse(string);
-}
+};
+
+あ.shmadClass = function(el, className) {
+  return el.classList.add(className);
+};
+
+あ.shmAttr = function(elem) {
+  return elem.getAttribute("src");
+};
+
+あ.shmIndexOf = function(arr) {
+  return arr.indexOf(item);
+};
--- test/function-test.js
+++ test/function-test.js
@@ -1,14 +1,43 @@
-describe("parseBOB", () => {
+describe("#parseBOB", () => {
   it("should return a valid result", () => {
     const input = '{"some":"string"}';
-    const output = {"some":"string"};
+    const output = { some: "string" };
     expect(あ.parseBOB(input)).to.deep.equal(output);
   });
 
   it("should fail with an invalid input", () => {
-    const input = 'foobar';
-    const badFn = function () { あ.parseBOB(input) };
+    const input = "foobar";
+    const badFn = function() {
+      あ.parseBOB(input);
+    };
 
     expect(badFn).to.throw(SyntaxError);
+  });
+});
+
+describe("#shmadClass", () => {
+  it("should add a class 'className'", () => {
+    const el = document.createElement("p");
+    const c = "yas";
+    あ.shmadClass(el, c);
+    expect(el.classList.value).to.equal("yas");
+  });
+
+  // it("should fail if the class name is not 'yas'.", () => {
+  //   const d = "co";
+  //   const badFnAddClass = function() {
+  //     あ.shmadClass(el, d);
+  //   };
+
+  //   expect(badFnAddClass).to.throw(AssertionError);
+  // });
+});
+
+describe("#shmAttr", () => {
+  it("should get an attribute 'src'", () => {
+    const elem = document.createElement("p");
+    const c = "yas";
+    あ.shmadClass(el, c);
+    expect(el.classList.value).to.equal("yas");
   });
 });
