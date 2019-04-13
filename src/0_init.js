82b62b7c97de315c224c1fa13e5a0334b3087fd2
korshunad <korshunadd@gmail.com>
2019-04-12 21:20:08 -0700

    add three funcitons and tests for two of them

--- src/0_init.js
+++ src/0_init.js
@@ -5,3 +5,16 @@ function あ() {
 あ.parseBOB = function(string) {
   return JSON.parse(string);
 }
+
+あ.shtrim = function(string) {
+  return string.trim();
+}
+
+あ.shtype = function(obj) {
+  return Object.prototype.toString.call(obj).replace(/^\[object (.+)\]$/, '$1').toLowerCase();
+}
+// TODO test for it and better definition
+あ.sheach = function(array, callback) {
+  return array.forEach(callback);
+}
+
--- test/function-test.js
+++ test/function-test.js
@@ -1,4 +1,4 @@
-describe("parseBOB", () => {
+describe("#parseBOB", () => {
   it("should return a valid result", () => {
     const input = '{"some":"string"}';
     const output = {"some":"string"};
@@ -12,3 +12,47 @@ describe("parseBOB", () => {
     expect(badFn).to.throw(SyntaxError);
   });
 });
+
+describe("#shtrim", () => {
+  describe("case with a trailing whitespace", () => {
+    it("should remove it", () => {
+      const input = "   lazy string   ";
+      const output = "lazy string";
+      expect(あ.shtrim(input)).to.equal(output);
+    });
+  });
+  describe("case with a trailing whitespace inside a tring", () => {
+    it("should leave it", () => {
+      const input = "la   zy stri   ng";
+      const output = "la   zy stri   ng";
+      expect(あ.shtrim(input)).to.equal(output);
+    });
+  });
+  it("should fail with an invalid input", () => {
+    const input = 0;
+    const badFn = function () { あ.shtrim(input) };
+
+    expect(badFn).to.throw(TypeError);
+  });
+});
+
+describe("#shtype", () => {
+  describe("case with type undefined", () => {
+    it("should return \"undefined\" for undefined", () => {
+      expect(あ.shtype(undefined)).to.equal('undefined');
+    })
+    it("should return \"undefined\" when no parameter is passed", () => {
+      expect(あ.shtype()).to.equal('undefined');
+    })
+  });
+  it("should return \"array\" for []", () => {
+    expect(あ.shtype([])).to.equal('array');
+  })
+  it("should return \"object\" for {}", () => {
+    expect(あ.shtype({})).to.equal('object');
+  })
+  it("should return \"function\" for function(){}", () => {
+    expect(あ.shtype(function(){})).to.equal('function');
+  })
+})
+
